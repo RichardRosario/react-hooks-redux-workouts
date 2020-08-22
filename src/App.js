ca8bd5a6ed77e3e6cef791072ace7ee1bb134ec8
Richard Rosario <tech2js@gmail.com>
2020-08-22 11:31:32 +0800

    working the useState hook

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(2);
-
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div>
-      <button onClick={() => setCount((currentCount) => currentCount + 1)}>
-        +
-      </button>
+      <button onClick={() => setCount((bilangin) => bilangin + 1)}>+</button>
       {count}
+      <input name="name" id="name" type="text" onChange={() => setName} />
+      <input name="email" id="email" type="text" onChange={() => setEmail} />
+      <input
+        name="password"
+        id="password"
+        type="password"
+        onChange={() => setPassword}
+      />
+      {(name, email, password)}
     </div>
   );
 };
